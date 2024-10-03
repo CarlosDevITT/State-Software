b6e8068b7b9eb4aeb0183fdb8652fca31758e36d
CarlosDevITT <carlosdomingos93@gmail.com>
2024-10-02 21:51:36 -0400

    Versão teste #01 - Modificador index do projeto , layout atualizador e responsivel, mudança no estilo do projeto.

--- script.js
+++ script.js
@@ -1,81 +1,10 @@
-let list = document.querySelectorAll('.list .item');
-
-list.forEach(item => {
-  item.addEventListener('click', function(event) {
-    if (event.target.classList.contains('add')) {
-      const itemNew = item.cloneNode(true);
-      let existsInCart = false;
-
-      let cartItems = document.querySelectorAll('.cart .item');
-      cartItems.forEach(cart => {
-        if (cart.getAttribute('data-key') == itemNew.getAttribute('data-key')) {
-          existsInCart = true;
-          cart.classList.add('danger');
-          setTimeout(function() {
-            cart.classList.remove('danger');
-          }, 1000);
-        }
-      });
-
-      if (!existsInCart) {
-        document.querySelector('.listCart').appendChild(itemNew);
-      }
-    }
-  });
-});
-
-function removeItem(key) {
-  let cartItems = document.querySelectorAll('.cart .item');
-  cartItems.forEach(item => {
-    if (item.getAttribute('data-key') == key) {
-      item.remove();
-      return;
-    }
-  });
-}
-
- function finalizePurchase() {
-
-    // Obter os itens do carrinho
-
-    const cartItems = document.querySelectorAll('.listCart .item');
-
-
-    // Verificar se há itens no carrinho
-
-    if (cartItems.length > 0) {
-
-      // Criar uma mensagem de confirmação
-
-      const confirmationMessage = 'Você deseja finalizar a compra com os seguintes itens:\n';
-
-
-      // Iterar sobre os itens do carrinho e adicionar à mensagem de confirmação
-
-      cartItems.forEach(item => {
-
-        confirmationMessage += `* ${item.getAttribute('data-name')}\n`;
-
-      });
-
-
-      // Mostrar a mensagem de confirmação
-
-      alert(confirmationMessage);
-
-
-      // Limpar o carrinho
-
-      cartItems.forEach(item => {
-
-        item.remove();
-
-      });
-
-    } else {
-
-      alert('O carrinho está vazio!');
-
-    }
-
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+  } else {
+      menuMobile.classList.add('open');
+      document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
   }
+}
