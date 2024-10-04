259197c5a36662bd40ea59fa2c14679e1e0d1614
CarlosDevITT <carlosdomingos93@gmail.com>
2024-10-04 11:54:25 -0400

    Versão teste #01 - Modificado layout do index , desgin responsivel para mobile

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+window.addEventListener("scroll", function(){
+  let header = document.querySelector('#header')
+  header.classList.toggle('rolagem',window.scrollY > 0)
+})
+
 function menuShow() {
   let menuMobile = document.querySelector('.mobile-menu');
   if (menuMobile.classList.contains('open')) {
