5c5f45da7fdb865fce93819582781fb6cd0cdcc7
CarlosDevITT <carlosdomingos93@gmail.com>
2024-10-16 17:52:02 -0400

    VERSÃO TESTE #01 - LAYOUT ATUALIZADO , MODIFICAÇAO NA PASTA DO PROJETO!

--- script.js
+++ script.js
@@ -13,3 +13,49 @@ function menuShow() {
       document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
   }
 }
+
+const topo = document.querySelector(".topo-do-site");
+const image= document.querySelector(".img-topo-site");
+
+const cursor ={
+  isdragging: false,
+  initialPosition: 0,
+
+};
+
+let currentImage = 1;
+
+const updateImage = (direction) => {
+   if(direction < 0 ) {
+     
+    currentImage += 1;
+
+   }
+   if(direction > 0) {
+     currentImage -= 1
+   }
+     imagem.src = './img/${currentImage}.png'
+};
+
+topo.addEventListener("mouse", (event) => {
+  cursor.isdragging = true;
+  cursor.initialPosition = event.clientX;
+})
+topo.addEventListener("mouseup", () =>{
+  curso.isdragging = false;
+});
+
+topo.addEventListener ("mousemove", ({ clientX }) => {
+  if (!cursor.isdragging) return;
+
+  const offest = curso.initialPosition - clientX;
+
+  if(Math.abs(offest) >= 50){
+    updateImage(offset);
+    cursor.initialPosition = clientX;
+  }
+
+
+    
+  
+})
